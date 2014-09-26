f77ead0d1b22ebf141a2d1e1f26c5ce938e78850
Allan Baños <Allan@MacBook-Pro-de-Allan.local>
2014-09-25 19:47:34 -0500

    Initial Commit

---
+++ Lab04/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab04
+//
+//  Created by Allan Baños on 25/09/14.
+//  Copyright (c) 2014 Edgar Allan Baños Romero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
